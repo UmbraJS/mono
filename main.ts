cba59c0e9aa8d5bbb089e9a8f0b3e13cc5fa05ef
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2023-03-18 17:33:06 +0100

    adds playground

---
+++ main.ts
@@ -0,0 +1,34 @@
+import {
+  myriad,
+  getReadable,
+  makeReadable,
+  Myriad
+} from "."
+
+function linkColor(m: Myriad) {
+  if(!m.foreground) return "black"
+  const blue = '#6b6bff'
+  const linkColor = makeReadable(blue, m, 7)
+  return linkColor
+}
+
+function imgColor(m: Myriad) {
+  if(!m.foreground) return "black"
+  return getReadable(m.foreground, 'black', 19)
+}
+
+
+myriad({
+  background: '#0c0915',
+  foreground: '#c0aea3',
+  accents: ['#c97074'],
+  custom: {
+    link: linkColor,
+    imgColor: imgColor,
+    success: '#00ff00',
+    error: '#ff0000',
+  }
+})
+
+export const some = 0;
+
