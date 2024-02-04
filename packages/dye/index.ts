57981e5a05b9126307248cbb4ce0d777dac12e14
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2024-02-04 16:23:16 +0100

    export new parts

--- packages/dye/index.ts
+++ packages/dye/index.ts
@@ -1,7 +1,11 @@
 import DyePicker from './components/DyePicker.vue'
+import DyeWrapper from '@components/DyeWrapper.vue'
+import Pallet from './components/Pallet.vue'
 import ColorCanvas from './components/Canvas/ColorCanvas.vue'
 import HueCanvas from './components/Canvas/HueCanvas.vue'
-import Pallet from './components/Pallet.vue'
 import Handle from './components/Handle.vue'
+import { useDye } from './composables/useDye'
+import { OutputColor } from './composables/canvas'
 
-export { DyePicker, ColorCanvas, HueCanvas, Pallet, Handle }
+export { DyePicker, DyeWrapper, ColorCanvas, HueCanvas, Pallet, Handle, useDye }
+export type { OutputColor }
