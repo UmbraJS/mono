d70a15dfbf28de7d5ad92a3fe13fcb14acb254d5
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2024-04-30 12:31:33 +0200

    adds ui

--- packages/docs/app.vue
+++ packages/docs/app.vue
@@ -4,6 +4,13 @@ import DyePicker from './components/DyePicker.vue'
 
 <template>
   <DyePicker color="#ff0000" />
+  <UDashboardCard
+    title="Recent sales"
+    description="You made 265 sales this month."
+    icon="i-heroicons-chart-bar"
+  >
+    <UProgress />
+  </UDashboardCard>
 </template>
 
 <style lang="scss">
--- packages/docs/nuxt.config.ts
+++ packages/docs/nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt']
+  extends: ['@nuxt/ui-pro'],
+  modules: ['@pinia/nuxt', '@nuxt/ui']
 })
