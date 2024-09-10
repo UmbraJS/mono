c53896447ef70ff9c5cb6c9067a28bdc906a66c2
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2024-09-10 15:07:55 +0200

    Delete unused files and update package dependencies

--- packages/nobel/src/pages/CarbonBoard.vue
+++ packages/nobel/src/pages/CarbonBoard.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import CarbonFiber from '../packages/CarbonFiber/index.vue'
+// import CarbonFiber from '../packages/CarbonFiber/index.vue'
 </script>
 
 <template>
-  <CarbonFiber />
+  <!-- <CarbonFiber /> -->
+  <div>carbon fiber</div>
 </template>
