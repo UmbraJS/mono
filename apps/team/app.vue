9a018fc136d84516ab9fa8858f5cfb39d93ed712
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2024-10-19 22:34:52 +0200

    Refactor Slider component to include zoom functionality and improve handle movement and size calculation

--- apps/team/app.vue
+++ apps/team/app.vue
@@ -1,5 +1,71 @@
+<script setup lang="ts">
+import '@nobel/core/styles/main.scss'
+import { umbra } from '@umbrajs/core'
+import type { Accent } from '@umbrajs/core'
+
+const warningAccent: Accent = {
+  name: 'warning',
+  color: '#ff0000'
+}
+
+const successAccent: Accent = {
+  name: 'success',
+  color: '#00ff00'
+}
+
+const theme = umbra({
+  background: '#ffffff',
+  foreground: '#000000',
+  accents: ['#8888ff', warningAccent, successAccent]
+})
+</script>
+
 <template>
   <div>
     <NuxtPage />
   </div>
 </template>
+
+<style>
+header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+nav {
+  display: flex;
+  gap: var(--space-3);
+  justify-content: center;
+  margin-bottom: var(--space-3);
+}
+
+nav a {
+  text-decoration: none;
+  color: var(--base-120);
+}
+
+nav a:hover {
+  color: var(--accent-50);
+}
+
+nav a.router-link-active {
+  color: var(--accent-60);
+}
+
+body {
+  background: var(--base);
+  padding: var(--space-1);
+  height: 100%;
+}
+
+#app {
+  height: 100%;
+}
+
+main {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+}
+</style>
