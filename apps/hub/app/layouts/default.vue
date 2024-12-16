252c2ff9f64827ca66b1fd4fecb1d01d6148d7d3
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2024-12-16 02:49:30 +0100

    Refactor layout styles to use viewport dimensions for the underbar component

--- apps/hub/app/layouts/default.vue
+++ apps/hub/app/layouts/default.vue
@@ -42,9 +42,9 @@ onKeyStroke('Escape', () => {
 .underbar {
   position: fixed;
   top: 0;
-  bottom: 0;
   left: 0;
-  right: 0;
+  height: 100vh;
+  width: 100vw;
   background: var(--base-10);
 
   display: grid;
