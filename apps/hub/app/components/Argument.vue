a2c997538b6109b525584c8d52242390fb3ab6fa
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2025-01-01 18:30:07 +0100

    feat: add ReasonConclution component and refactor Argument component for improved structure and styling

--- apps/hub/app/components/Argument.vue
+++ apps/hub/app/components/Argument.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type { Reason } from '../types/reasons'
+import ReasonConclution from './ReasonConclution.vue'
 import ReasonMeta from './ReasonMeta.vue'
 
 const props = defineProps<{
@@ -8,35 +9,38 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div class="argument">
-    <!-- <NuxtImg
-      src="https://images.unsplash.com/photo-1731331131233-4f73c93ae693?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-      alt=""
-    /> -->
-    <div class="conclution card">
-      <p>{{ props.reason.conclution }}</p>
-      <div class="label">
-        <p>
-          {{ props.reason.credibility.level }}
-          <span>{{ props.reason.credibility.value }}</span>
-        </p>
-      </div>
+  <div class="argument bodycopy">
+    <header>
+      <ReasonConclution :reason="props.reason" />
+      <NuxtImg
+        src="https://images.unsplash.com/photo-1731331131233-4f73c93ae693?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt=""
+      />
+    </header>
+    <div class="argument-content">
+      <!-- <ReasonMeta :reason="props.reason" /> -->
+      <slot />
     </div>
-    <!-- <ReasonMeta :reason="props.reason" /> -->
-    <slot />
   </div>
 </template>
 
 <style>
 .argument {
+  display: flex;
+  flex-direction: column;
+  background-color: var(--base-20);
+  border: solid var(--border-size) var(--base-60);
+  border-radius: var(--radius);
+  overflow: hidden;
+}
+
+.argument-content {
   position: relative;
   display: flex;
   flex-direction: column;
   gap: var(--space-1);
-  background-color: var(--base-20);
-  border: solid var(--border-size) var(--base-60);
   padding: var(--space-2);
-  border-radius: var(--radius);
+  padding-top: var(--space-3);
 }
 
 .argument .card {
@@ -47,18 +51,28 @@ const props = defineProps<{
   transition: var(--slower);
 }
 
-.conclution {
+.argument header {
+  position: relative;
   display: flex;
-  justify-content: space-between;
-  align-items: center;
+  flex-direction: column;
+  gap: var(--space-1);
+  padding: var(--space-2);
+  padding-top: var(--space-4);
+  padding-bottom: 0px;
+  border-bottom: solid var(--border-size) var(--base-60);
 }
 
-.conclution .label {
-  background: var(--base-20);
-  color: var(--base-120);
-  border-top-right-radius: var(--radius);
-  border-bottom-right-radius: var(--radius);
-  padding: var(--space-1) var(--space-2);
-  border-left: solid var(--border-size) var(--base-60);
+.argument header img {
+  position: absolute;
+  z-index: 0;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  overflow: hidden;
+  border-radius: var(--radius);
+  border-bottom-right-radius: 0px;
+  border-bottom-left-radius: 0px;
 }
 </style>
---
+++ apps/hub/app/components/ReasonConclution.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import type { Reason } from '../types/reasons'
+
+const props = defineProps<{
+  reason: Reason
+}>()
+</script>
+
+<template>
+  <div class="conclution card">
+    <p>{{ props.reason.conclution }}</p>
+    <div class="label">
+      <p>
+        {{ props.reason.credibility.level }}
+        <span>{{ props.reason.credibility.value }}</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<style>
+.conclution {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+  z-index: 1;
+  transform: translateY(50%);
+}
+
+.conclution .label {
+  background: var(--base-20);
+  color: var(--base-120);
+  border-top-right-radius: var(--radius);
+  border-bottom-right-radius: var(--radius);
+  padding: var(--space-1) var(--space-2);
+  border-left: solid var(--border-size) var(--base-60);
+}
+</style>
