2bb1ef612c6c30e2dd8faeec67eb0f3bdfb0b227
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2025-02-26 22:09:57 +0100

    removes unused Form component; enhances Input component with improved styling, label support, and focus effects for better usability

--- packages/nobel/components/ui/Form.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <form>
-    <slot></slot>
-  </form>
-</template>
-
-<style lang="scss"></style>
--- packages/nobel/components/ui/Input.vue
+++ packages/nobel/components/ui/Input.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, useModel } from 'vue'
+import { useVModel } from '@vueuse/core'
+
+defineOptions({
+  inheritAttrs: false,
+})
 
 const { size = 'medium' } = defineProps<{
   size?: 'medium' | 'small' | 'mini'
   label: string
 }>()
+
+const emit = defineEmits(['update:value'])
 
 const focused = ref(false)
 </script>
@@ -19,10 +26,11 @@ const focused = ref(false)
       {{ label }}
     </label>
     <input
+      v-bind="$attrs"
       :id="label"
       ref="inputRef"
       class="button buttonText buttonHover buttonActive buttonFocus focus"
-      :class="size"
+      :class="`${size}`"
       :placeholder="label"
       @focus="() => (focused = true)"
       @blur="() => (focused = false)"
@@ -40,10 +48,25 @@ input {
 /* input:-internal-autofill-selected {
   background-color: var(--base-20) !important;
   color: var(--base-contrast) !important;
+}
+
+input:-webkit-autofill {
+  background-color: var(--base-20) !important;
+  color: var(--base-contrast) !important;
+}
+
+input:-webkit-autofill::placeholder {
+  background-color: var(--base-20) !important;
+  color: var(--base-contrast) !important;
 } */
 
 .input {
   position: relative;
+}
+
+div.input:has(input:not(:placeholder-shown)):not(:focus-within) label {
+  transform: translateY(0%);
+  opacity: 0;
 }
 
 div.input:has(+ div:focus-within) {
@@ -61,6 +84,15 @@ div.input:has(+ div:focus-within) {
   padding: 0 var(--space-quark);
   pointer-events: none;
   transition: all 0.3s;
+}
+
+.input input.focus:focus {
+  color: var(--accent-120);
+  background: var(--accent-10);
+}
+
+.input input::placeholder {
+  color: var(--accent-120);
 }
 
 .input label.move {
