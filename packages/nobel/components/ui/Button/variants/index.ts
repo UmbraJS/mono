ad491f8f09edc06f41e3a515e6411609bf4a5d94
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2025-04-17 19:48:34 +0200

    feat: add button variants and types for improved flexibility and customization

---
+++ packages/nobel/components/ui/Button/variants/index.ts
@@ -0,0 +1,9 @@
+import ButtonPrimary from './ButtonSecondary.vue'
+import ButtonSecondary from './ButtonSecondary.vue'
+import ButtonBase from './ButtonBase.vue'
+
+export const buttonVariants = {
+  primary: ButtonPrimary,
+  secondary: ButtonSecondary,
+  base: ButtonBase,
+}
