e8d7090bc4a481b547c623802c151941c9c04d4b
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2025-04-20 17:17:34 +0200

    feat: add Graph component for data visualization with customizable axes and tooltips

---
+++ packages/nobel/components/graph/Graph.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import { VisXYContainer, VisLine, VisAxis, VisCrosshair, VisTooltip } from '@unovis/vue'
+
+type DataRecord = {
+  x: number
+  y: number
+  y1: number
+  y2: number
+}
+
+defineProps<{ data: DataRecord[] }>()
+const padding = {
+  top: 0,
+  bottom: 0
+}
+const x = (d: DataRecord) => d.x
+const y = [
+  (d: DataRecord) => d.y,
+  (d: DataRecord) => d.y1,
+  (d: DataRecord) => d.y2
+]
+
+// const attributes = [
+//   { key: 'y', type: 'line' },
+//   { key: 'y1', type: 'line' },
+//   { key: 'y2', type: 'line' },
+// ]
+
+// const tooltipContent = (items: any[]) => {
+//   // items will include all lines hovered at that x
+//   return `
+//     <div>
+//       ${items.map((item) => {
+//     return `<div><strong>${item.key || 'Value'}:</strong> ${item.datum[item.attribute]}</div>`
+//   }).join('')}
+//     </div>
+//   `
+// }
+</script>
+
+<template>
+  <VisXYContainer :padding="padding" :data="data">
+    <!-- <VisTooltip :content="tooltipContent" /> -->
+    <VisLine :x="x" :y="y" />
+    <VisAxis type="x" />
+    <VisAxis type="y" />
+    <VisCrosshair />
+  </VisXYContainer>
+</template>
+
+<style>
+:root {
+  --vis-font-family: Inter, Arial, "Helvetica Neue", Helvetica, sans-serif;
+  --vis-color-main: #4D8CFD;
+  --vis-color-main-light: #d0e0fe;
+  --vis-color-main-dark: #3c588a;
+  --vis-color-grey: #2a2a2a;
+
+  --vis-color0: var(--warning-100);
+  --vis-color1: var(--success-100);
+  --vis-color2: var(--info-100);
+  --vis-color3: var(--yellow-100);
+  --vis-color4: #00C19A;
+  --vis-color5: #6859BE;
+}
+</style>
