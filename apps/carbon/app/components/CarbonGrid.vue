c7c28538889289c746d38eaa9590a8e845063adc
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2025-07-05 13:11:33 +0200

    Add CarbonGrid component for responsive grid layout with event and match configurations

---
+++ apps/carbon/app/components/CarbonGrid.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+defineProps<{
+  gridLayout: 'event' | 'match'
+}>()
+</script>
+
+
+<template>
+  <main id="CarbonGrid" :class="gridLayout">
+    <slot />
+  </main>
+</template>
+
+<style>
+#CarbonGrid {
+  --side-size: 17vh;
+
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  gap: var(--space-1);
+
+  height: 100vh;
+  width: 100%;
+  padding: var(--space-1);
+}
+
+#CarbonGrid.event {
+  grid-template-rows: 1fr var(--side-size);
+}
+
+#CarbonGrid.match {
+  grid-template-rows: var(--side-size) 1fr var(--side-size);
+
+}
+</style>
