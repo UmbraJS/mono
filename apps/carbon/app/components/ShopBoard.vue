92ddfea0e5ec88feec93cf4d09f4d90b73c35494
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2025-07-05 13:11:39 +0200

    Add ShopBoard component for displaying player cards in a grid layout; create Drawer component for future UI enhancements.

---
+++ apps/carbon/app/components/ShopBoard.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import PlayerCard from '~/components/Card/Card.vue'
+const quest = useQuest()
+</script>
+
+<template>
+  <div id="ShopBoard">
+    <PlayerCard v-for="event in quest.currentShop" :key="event.id" :card="event" :placement="false" />
+  </div>
+</template>
+
+<style>
+#ShopBoard {
+  display: grid;
+  grid-template-columns: subgrid;
+  gap: var(--space-1);
+  grid-column: 1 / -1;
+  height: 150px;
+}
+</style>
