555ad67af2e8d4f2ff3b1c7ca4a9f4be843294d2
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2025-07-19 01:42:01 +0200

    Add Buyback component: implement UI for buying back sold cards with error handling

---
+++ apps/carbon/app/components/Button/Buyback.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { Drawer } from '@nobel/core'
+const store = useStore()
+</script>
+
+<template>
+  <Drawer class-name="viewButton" title="Buyback Store" description="Buy back your previously sold cards">
+    <template #trigger>
+      <Icon name="carbon:money" size="1.5em" />
+      <p>buyback</p>
+    </template>
+    <template #content>
+      <div class="content">
+        <!-- <DrawerTitle id="drawer-buyback-title">
+                Buyback
+              </DrawerTitle> -->
+        <!-- <DrawerDescription id="drawer-buyback-description">
+                Buy back your sold cards.
+              </DrawerDescription> -->
+        <div id="BuyBackCards">
+          <div v-if="store.money.cardPurchase.purchaseError">
+            <Icon name="carbon:warning" size="1rem" />
+            <p>{{ store.money.cardPurchase.purchaseError }}</p>
+          </div>
+          <div v-for="(card, index) in store.money.soldCards" :key="index" class="card">
+            <CardHeader :card="card" variant="cardSize" @click="() => {
+              store.money.buyBackCard(card)
+            }" />
+          </div>
+        </div>
+      </div>
+    </template>
+  </Drawer>
+</template>
