f3346a42af784808422e06817c7c0c15e7205a0d
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2025-07-19 02:32:34 +0200

    Add ShopAvatar and ShopHeader components: implement UI for shop display with structured layout and dynamic content

---
+++ apps/carbon/app/components/ShopAvatar.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const quest = useQuest()
+</script>
+
+<template>
+  <div v-if="quest.shop.current" id="ShopAvatar">
+    <NuxtImg v-if="quest.shop.current.images.inside" class="avatar" :src="quest.shop.current.images.inside"
+      :alt="quest.shop.current.description" width="200" height="200" placeholder format="webp" fit="cover" />
+  </div>
+</template>
+
+<style>
+#ShopAvatar {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--base-40);
+  padding: var(--space-1);
+  border-radius: var(--radius);
+}
+
+#ShopAvatar .avatar {
+  width: 100%;
+  height: 100%;
+  border-radius: var(--radius);
+}
+</style>
---
+++ apps/carbon/app/components/ShopHeader.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { Button } from '@nobel/core'
+const quest = useQuest()
+</script>
+
+<template>
+  <header v-if="quest.shop.current" id="ShopHeader">
+    <ShopAvatar />
+    <div id="ShopMetaInfo">
+      <h2>{{ quest.shop.current.name }}</h2>
+      <p>{{ quest.shop.current.description }}</p>
+
+      <div id="ShopActions">
+        <Button variant="base">
+          <Icon name="carbon:arrow-left" size="1em" />
+          <p>Exit</p>
+        </Button>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+#ShopHeader {
+  display: grid;
+  grid-template-columns: auto 1fr;
+  gap: var(--space-2);
+
+  background-color: var(--base);
+  padding: var(--space-2);
+  border-radius: var(--radius);
+}
+
+#ShopMetaInfo {
+  display: grid;
+  gap: var(--space-1);
+  grid-template-rows: auto 1fr auto;
+}
+
+#ShopActions {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
