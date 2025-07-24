fb095972a168d1415c4f83702e39dc20daece068
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2025-07-25 00:12:34 +0200

    Add ChipMessage component with template and styles

---
+++ apps/carbon/app/components/Chip/ChipMessage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="ChipMessage" class="border">
+    <slot />
+  </div>
+</template>
+
+<style>
+#ChipMessage {
+  display: flex;
+  align-items: center;
+  gap: var(--space-1);
+  padding: var(--space-2);
+  color: var(--base-120);
+  background-color: var(--base-10);
+  border-radius: var(--radius);
+}
+</style>
