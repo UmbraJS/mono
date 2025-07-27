04bdfda0be9dc5e849dba6c0142b1ea739a1095c
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2025-07-27 23:22:19 +0200

    Add CharacterModal and DrawerButton components for improved UI interaction

---
+++ apps/carbon/app/components/CharacterModal.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { Drawer } from '@nobel/core'
+
+import type { Character } from '../../types'
+
+defineProps<{
+  character: Character;
+}>()
+</script>
+
+<template>
+  <Drawer title="Player Settings" description="Configure your character settings and preferences">
+    <template #trigger>
+      <slot />
+    </template>
+    <template #content>
+      <div class="DialogWrapper">
+        <h1>{{ character.name }}</h1>
+      </div>
+    </template>
+  </Drawer>
+</template>
+
+<style></style>
---
+++ packages/nobel/components/ui/Drawer/DrawerButton.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Drawer from './Drawer.vue'
+import Button from '../Button/Button.vue'
+
+defineProps<{
+  className?: string
+}>()
+</script>
+
+<template>
+  <Drawer>
+    <template #trigger>
+      <Button :class="className" color="default" variant="base">
+        <slot name="trigger" />
+      </Button>
+    </template>
+    <template #content>
+      <slot name="content" />
+    </template>
+  </Drawer>
+</template>
