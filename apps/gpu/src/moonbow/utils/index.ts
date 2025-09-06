2722cfab57c3ea092898b7efd409e28058225682
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2025-09-06 13:36:54 +0200

    gg

--- apps/gpu/src/moonbow/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export function isFunc<T extends (...args: any) => any>(val: unknown): val is T {
-  return typeof val === 'function'
-}
--- packages/moonbow/src/pipeline/entries.ts
+++ packages/moonbow/src/pipeline/entries.ts
@@ -1,5 +1,4 @@
 import type { UniformBuffer } from '../buffers'
-import type { ResourceBinding } from '../buffers/textures'
 
 export function getUniformEntries(props: { device: GPUDevice; uniforms: UniformBuffer[] }) {
   const defaultVisibility = GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT | GPUShaderStage.COMPUTE
