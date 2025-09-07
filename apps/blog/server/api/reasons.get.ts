898a6ae218fc5f5017f0127b07626d0f9360c7df
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2025-09-07 17:42:02 +0200

    reasons

---
+++ apps/blog/server/api/reasons.get.ts
@@ -0,0 +1,8 @@
+import type { Reason } from '../../types/reasoning'
+import data from '../../app/data'
+
+export default defineEventHandler(() => {
+  // Return the reasons array used by the blog components.
+  // data.ts exports a default object: { user, reasons }
+  return data.reasons as Reason[]
+})
