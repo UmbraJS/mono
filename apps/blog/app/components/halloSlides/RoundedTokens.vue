98008036463beeb87da2cc92e07453c15d81a5dc
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2025-09-30 19:59:01 +0200

    rounded

---
+++ apps/blog/app/components/halloSlides/RoundedTokens.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="SamSlide RoundedTokens">
+    <p class="display">
+      <span>--radius-1:</span> 00px;<br />
+      <span>--radius-2:</span> 10px;<br />
+      <span>--radius-3:</span> 30px;
+    </p>
+
+    <div class="RoundedExamples">
+      <div class="Example border"></div>
+      <div class="Example border"></div>
+      <div class="Example border"></div>
+    </div>
+  </div>
+</template>
+
+<style>
+.RoundedTokens {
+  --radius-1: 0px;
+  --radius-2: 10px;
+  --radius-3: 30px;
+
+  display: flex;
+  gap: var(--space-5);
+}
+
+.RoundedExamples {
+  transform: translate(0, -50px);
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 30em;
+}
+
+.Example {
+  position: absolute;
+  height: 100px;
+  width: 100%;
+  background: var(--base-20);
+}
+
+.Example:nth-child(1)::after,
+.Example:nth-child(2)::after {
+  content: '';
+  display: block;
+  height: 100%;
+  width: 100%;
+  box-sizing: border-box;
+  background-color: var(--base);
+  transform: translate(25px, 30px);
+  filter: blur(20px);
+}
+
+.Example:nth-child(1) {
+  border-top-left-radius: var(--radius-1);
+}
+
+.Example:nth-child(2) {
+  border-top-left-radius: var(--radius-2);
+  transform: translate(50px, 50px);
+}
+
+.Example:nth-child(3) {
+  border-top-left-radius: var(--radius-3);
+  transform: translate(100px, 100px);
+}
+</style>
---
+++ apps/blog/app/pages/helloStavanger.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { onKeyStroke } from '@vueuse/core';
+import RoundedTokens from '../components/halloSlides/RoundedTokens.vue';
+
+const slide = ref(1);
+
+onKeyStroke('ArrowRight', () => {
+  console.log('right');
+  slide.value++;
+});
+
+onKeyStroke('ArrowLeft', () => {
+  console.log('left');
+  slide.value--;
+});
+
+
+</script>
+
+<template>
+  <div v-if="slide === 1" class="SamSlide">
+    <p class="display">
+      A design <span>system</span> isn’t UI components.
+      It isn’t dark mode <br /> or rounded corners.
+      It’s the <span>rules</span> that make <br /> those decisions possible.
+    </p>
+  </div>
+
+  <RoundedTokens v-if="slide === 2" class="SamSlide" />
+</template>
+
+<style scoped>
+.SamSlide {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
