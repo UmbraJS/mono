e05363454743eea0cb4047e16bf8599bc8b3f2a0
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2025-11-10 15:32:29 +0100

    Refactor RichText layout and add CaseHeader, AuthorBar
    Extracted CaseHeader and AuthorBar into separate components for improved modularity and maintainability. Updated RichText.vue to use these new components and removed related inline markup and styles.

---
+++ apps/blog/app/components/AuthorBar.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import type { author as authorType } from '../../types/profile'
+import IdentityTag from './IdentityTag.vue'
+
+defineProps<{
+  author: typeof authorType
+}>()
+</script>
+
+<template>
+  <div class="AuthorBar">
+    <NuxtImg class="AuthorAvatar" :src="author.avatarUrl" width="75" height="75" alt="Author avatar" />
+    <div class="AuthorInfo">
+      <p>
+        <span>
+          {{ author.name }}
+        </span>
+      </p>
+      <div class="MandatoryIdentityFields">
+        <IdentityTag v-for="tag in author.indentityTags" :key="tag.id" :identity-tag="tag" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.MandatoryIdentityFields {
+  display: grid;
+  grid-auto-flow: column;
+  gap: var(--space-quark);
+}
+
+.AuthorInfo {
+  display: flex;
+  flex-direction: column;
+  gap: var(--space-1);
+}
+
+.AuthorBar {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: var(--space-2);
+  background-color: var(--base-20);
+  padding: var(--space-1);
+  border-radius: var(--radius);
+  width: 100%;
+  max-width: clamp(300px, 100vw, 700px);
+}
+
+.AuthorBar .AuthorAvatar {
+  border-radius: var(--radius);
+  object-fit: cover;
+}
+</style>
---
+++ apps/blog/app/components/CaseHeader.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { EditorContent } from '@tiptap/vue-3'
+import type { Editor } from '@tiptap/vue-3'
+
+defineProps<{
+  titleEditor: Editor | undefined
+  imageUrl: string
+}>()
+</script>
+
+<template>
+  <div class="CaseHeader">
+    <NuxtImg :src="imageUrl" width="1200" height="1000" alt="Case header image" />
+    <EditorContent :editor="titleEditor" class="CaseTitle" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.CaseTitle {
+  position: relative;
+  padding: var(--space-2);
+  /* background-color: rgba($color: black, $alpha: 0.2); */
+  /* box-shadow: 0px -70px 54px 0px rgba(0, 0, 0, 1); */
+  backdrop-filter: blur(5px);
+}
+
+.CaseHeader {
+  position: relative;
+  height: 70vh;
+  display: flex;
+  align-items: flex-end;
+}
+
+.CaseHeader img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  overflow: hidden;
+  border-radius: var(--radius);
+}
+</style>
